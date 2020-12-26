add4081604fd4ca8a0fb0655d727940111af23f3
Alan O Brien <alanobrien777@gmail.com>
2020-12-26 23:54:00 +0000

    Topic 10 - Update Movies Data/Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import './db';
-import {loadUsers} from './seedData';
+import {loadUsers, loadMovies} from './seedData';
 import usersRouter from './api/users';
 import session from 'express-session';
 import passport from './authenticate';
@@ -26,6 +26,7 @@ const port = process.env.PORT;
 
 if (process.env.SEED_DB) {
     loadUsers();
+    loadMovies();
   }
 
 app.use(express.static('public'));
